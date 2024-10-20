8191d8c272c8f527230e89bb880c86757d925b95
takumi <51111242+takumi3488@users.noreply.github.com>
2024-10-21 01:25:24 +0900

    update: ファイルが無い場合の処理に対応

--- src/epub/images.rs
+++ src/epub/images.rs
@@ -20,8 +20,8 @@ pub fn sort_image_files(dir: &str) -> Vec<Image> {
     let target_files = glob(format!("{}/**/*", dir).as_str()).unwrap();
     let re = Regex::new(r"/(\D*|.*\D)(\d{1,6})\.(jpe?g|JPE?G|png|PNG|webp|WEBP)$").unwrap();
     let mut sorted_files = target_files
-        .map(|x| x.unwrap())
-        .filter(|x| re.is_match(x.to_str().unwrap()))
+        .filter_map(|x| x.ok())
+        .filter(|x| re.is_match(x.to_str().unwrap_or_default()))
         .collect::<Vec<_>>();
     sorted_files.sort_by(|a, b| {
         let a = re
